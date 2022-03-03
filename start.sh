a1d803e9fb973e1faf042dd8811cbfa3f156e9ca
Marten Seemann <martenseemann@gmail.com>
2022-03-03 11:29:45 +0400

    pass the domain name to the coredns container

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# escape . in the domain name with a \
+domain_escaped=$(echo "$DOMAIN" | sed "s/\./\\\\\\\./g")
+
 # coredns doesn't like unresolved hostnames in forward directives
 # resolve the IP address that Docker assigned to the powerdns container now
-sed "s/<coredns>/$(dig +short powerdns)/g" Corefile.tmpl > Corefile
+sed "s/<coredns>/$(dig +short powerdns)/g" Corefile.tmpl | sed "s/<domain_escaped>/$domain_escaped/g" | sed "s/<domain>/$DOMAIN/g" > Corefile
 
 /coredns
