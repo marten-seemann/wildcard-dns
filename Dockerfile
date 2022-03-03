2bc0789854057704995d1d4131f37291ee875dbf
Marten Seemann <martenseemann@gmail.com>
2022-03-03 16:12:36 +0400

    make the SOA configurable

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,19 @@
 FROM golang:1.17-alpine
 
-RUN go install github.com/coredns/coredns@v1.9.0
+RUN apk add git
+RUN git clone https://github.com/coredns/coredns --branch v1.9.0 --single-branch /coredns
+WORKDIR /coredns
+RUN go get github.com/coredns/records
+RUN echo "records:github.com/coredns/records" >> plugin.cfg
+RUN go generate
+RUN go build -o coredns
 
 FROM alpine:latest
 
 RUN wget https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh && chmod +x wait-for-it.sh
 
 RUN apk add bind-tools curl bash
-COPY --from=0 /go/bin/coredns /
+COPY --from=0 /coredns/coredns /
 ADD start.sh /
 RUN chmod +x start.sh
 ADD Corefile.tmpl /
--- start.sh
+++ start.sh
@@ -2,14 +2,14 @@
 
 # escape . in the domain name with a \
 domain_escaped=$(echo "$DOMAIN" | sed "s/\./\\\\\\\./g")
+timestamp=$(date +%s)
 
 # coredns doesn't like unresolved hostnames in forward directives
 # resolve the IP address that Docker assigned to the powerdns container now
-sed "s/<coredns>/$(dig +short powerdns)/g" Corefile.tmpl | sed "s/<domain_escaped>/$domain_escaped/g" | sed "s/<domain>/$DOMAIN/g" > Corefile
+sed "s/<powerdns>/$(dig +short powerdns)/g" Corefile.tmpl | sed "s/<domain_escaped>/$domain_escaped/g" | sed "s/<domain>/$DOMAIN/g" | sed "s/<timestamp>/$timestamp/g" | sed "s/<soa>/$SOA/g" > Corefile
 
 /wait-for-it.sh powerdns:8081 -t 30
 
-timestamp=$(date +%s)
 curl -X POST --data '{"name":"'$DOMAIN'.", "kind": "Master", "dnssec":false, "soa-edit":"'$timestamp'", "masters": [], "nameservers": ["ns1.'$DOMAIN'."]}' -s -H "X-API-Key: $APIKEY" http://powerdns:8081/api/v1/servers/localhost/zones
 
 /coredns
